328e9a369dc34e5348edbb96059242dacf6eb594
GitSiege7 <carljklug@icloud.com>
2026-04-19 21:35:38 -0400

    refactored faculty ratio filtering

--- genericSearch.go
+++ genericSearch.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func (cfg *apiConfig) handlerGenericSearch(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +23,9 @@ func (cfg *apiConfig) handlerGenericSearch(w http.ResponseWriter, r *http.Reques
 
 	// QUERY COLLECTION
 	var filter bson.D
+	var cursor *mongo.Cursor
+	var err error
+	var sort int = 0
 
 	switch req.QueryParam {
 	case "Instructor":
@@ -35,30 +38,50 @@ func (cfg *apiConfig) handlerGenericSearch(w http.ResponseWriter, r *http.Reques
 			Key: "$all", Value: bson.A{days},
 		}}}}
 	case "Faculty Ratio":
-		pValue, err := strconv.Atoi(req.ParamValue)
-		if err != nil {
-			respondWithError(w, 400, "Invalid parameter value input")
-			return
+		if req.ParamValue == "Most Support" {
+			sort = 1
+		} else {
+			sort = -1
 		}
-		filter = bson.D{{Key: "$expr", Value: bson.D{{
-			Key: "$gte", Value: bson.A{
-				bson.D{{
-					Key: "$divide", Value: bson.A{
-						"$enrollment", bson.D{{
-							Key: "$add", Value: bson.A{
-								"$totalTAs", 1,
-							}}},
-					},
-				}},
-				pValue,
-			},
-		}}}}
+		filter = bson.D{{Key: "enrollment", Value: bson.D{{Key: "$ne", Value: 0}}}}
 	}
 
-	cursor, err := cfg.coll.Find(context.TODO(), filter)
-	if err != nil {
-		respondWithError(w, 500, fmt.Sprintf("Failed to query collection: %s", err))
-		return
+	if sort == 0 {
+		cursor, err = cfg.coll.Find(context.TODO(), filter)
+		if err != nil {
+			respondWithError(w, 500, fmt.Sprintf("Failed to query collection: %s", err))
+			return
+		}
+	} else {
+		cursor, err = cfg.coll.Aggregate(context.TODO(), mongo.Pipeline{
+			// filter
+			{{
+				Key: "$match", Value: filter,
+			}},
+			// expression
+			{{
+				Key: "$addFields", Value: bson.D{{
+					Key: "ratio", Value: bson.D{{
+						Key: "$divide", Value: bson.A{
+							"$enrollment", bson.D{{
+								Key: "$add", Value: bson.A{
+									"$totalTAs", 1,
+								}}},
+						},
+					}},
+				}},
+			}},
+			// sort
+			{{
+				Key: "$sort", Value: bson.D{{
+					Key: "ratio", Value: sort,
+				}},
+			}},
+		})
+		if err != nil {
+			respondWithError(w, 500, fmt.Sprintf("Failed to query collection: %s", err))
+			return
+		}
 	}
 	defer cursor.Close(context.TODO())
 
